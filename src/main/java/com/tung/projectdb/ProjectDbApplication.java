ed8f8dddc9ad15e50d51a33a88a403db1106173d
tungnguyensnk <tung20194714@gmail.com>
2021-11-21 20:55:04 +0700

    init

--- src/main/java/com/tung/projectdb/ProjectDbApplication.java
+++ src/main/java/com/tung/projectdb/ProjectDbApplication.java
@@ -1,6 +1,7 @@
 package com.tung.projectdb;
 
 import com.tung.projectdb.model.Data;
+import com.tung.projectdb.model.DonHangRepository;
 import com.tung.projectdb.model.ItemRepository;
 import com.tung.projectdb.model.TaiKhoanRepository;
 import org.springframework.boot.SpringApplication;
@@ -18,6 +19,20 @@ public class ProjectDbApplication {
         Data.setItems(itemRepository.getAll());
         TaiKhoanRepository taiKhoanRepository = context.getBean(TaiKhoanRepository.class);
         Data.setTaiKhoans(taiKhoanRepository.findAll());
+        new Thread(() -> {
+            int i=0;
+            while (i<10000) {
+                context.getBean(DonHangRepository.class).findAll().forEach(donHang ->
+                        context.getBean(DonHangRepository.class).upTrangThai("Giao hàng thành công.",donHang.getId()));
+                System.out.println("oke");
+                try {
+                    i++;
+                    Thread.sleep(30000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
 }
